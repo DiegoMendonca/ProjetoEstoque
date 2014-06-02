406ff5b67af53c538c620eac47bc39afd406de51
Diego <santos_diegol@yahoo.com.br>
2014-06-02 10:12:57 -0300

    Adicionando a classe TelaPrincipal

---
+++ ProjetoEstoque/src/br/com/diego/estoque/view/TelaPrincipal.java
@@ -0,0 +1,17 @@
+package br.com.diego.estoque.view;
+
+import javax.swing.JFrame;
+
+public class TelaPrincipal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public TelaPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(600, 800);
+		setTitle("Tela Principal - Estoque");
+		setLocationRelativeTo(null);
+
+	}
+
+}
